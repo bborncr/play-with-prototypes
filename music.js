f83c5b9df08c4e14cba584693741f272c321fc7b
bborncr <bentley@crcibernetica.com>
2017-07-27 15:29:12 +0000

    cleanup

--- music.js
+++ music.js
@@ -6,7 +6,6 @@ function Library(name, creator) {
 	this.playlists = [];
 }
 
-
 Library.prototype.addPlaylist = function (Playlist) {
 	this.playlists.push(Playlist);
 }
@@ -68,21 +67,3 @@ myLibrary.playlists[0].totalDuration();
 console.log(myLibrary.playlists[0].avg);
 console.log(myLibrary.playlists[0].totalLength);
 console.log(myLibrary);
-
-// Work incrementally but by the end of it you should have defined multiple prototype functions:
-
-// Library
-// Playlist
-// Track
-// This one will be slightly different from the Week 1 in that it will have the following functionality:
-
-// A Library has a name and a creator (both strings)
-// These are the only things required in order to create a music library
-// A Library has many playlists (starts as an empty array)
-// Playlist objects can be added to an instance of a library
-// Each Playlist has a name which is required upon creation
-// A Playlist also has many tracks
-// A Track has a title, a rating (an integer from 1 to 5) and a length (integer in seconds) all of which are required when creating a new track
-// Each Playlist also has an overallRating function which will calculate the rating by averaging the rating of its tracks
-// Each Playlist also has a totalDuration function which will sum the duration of all of its tracks
-// Once defined, your code should use new to instantiate these functions to create instances of one library and then add new tracks and playlists to the library.
